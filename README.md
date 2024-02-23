73a6047308256bab26365a9ec99c2239eb26dd1f
Mica Perillo <micaelaperillo26@gmail.com>
2024-02-23 20:57:13 -0300

    code + readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# dolarBot
+# DolarBot
+
+DolarBot is a Discord bot designed to provide currency exchange rates for various types of dollars against the Argentine peso (ARS). It utilizes the Dolar API to fetch the latest exchange rates and responds to user commands within a Discord server.
+
+## Features
+- Currency Conversion: DolarBot retrieves the current exchange rates for 7 types of dollars against the Argentine peso (ARS) upon receiving the `!dolares` command.
+- Help Command: Users can use the `!help` command to receive instructions on how to use the bot and get information about the available commands.
+
+## License
+DolarBot is licensed under the MIT License.
+
+## Disclaimer
+DolarBot is a project created for educational purposes and should be used responsibly. The accuracy of exchange rates may vary and should not be considered financial advice.
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import discord
+from datetime import datetime
+import pytz
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('!help'):
+    await message.channel.send('''¡Hola! 👋 ¿Buscas información sobre el precio del dólar?
+Simplemente escribe `!dolares` para obtener el precio de 7 tipos de dólares en pesos, tanto para compra como para venta.''')
+
+  if message.content.startswith('!dolares'):
+    dollars = requests.get("https://dolarapi.com/v1/dolares").json()
+    msg = "💰 *Cotizacion del Dolar* 💰\n"
+    for dolar in dollars:
+      # Format: USD {Nombre} - Compra: {compra} - Venta: {venta}
+      msg += f"- USD {dolar['nombre']} - Compra: ${dolar['compra']} - Venta: ${dolar['venta']}\n"
+
+    # Convertir la cadena a un objeto datetime
+    fecha_ultima_actualizacion = dollars[0]['fechaActualizacion']
+    fecha_ultima_actualizacion = datetime.fromisoformat(
+        fecha_ultima_actualizacion.replace("Z", "+00:00"))
+
+    fecha_ultima_actualizacion = fecha_ultima_actualizacion.strftime(
+        "%d/%m/%Y @ %H:%M:%S")
+    msg += f"🕝 Ultima actualizacion: {fecha_ultima_actualizacion}"
+    await message.channel.send(msg)
+
+token = os.getenv("TOKEN") or ""
+client.run(token)
